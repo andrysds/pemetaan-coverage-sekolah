89445f5c9bffe8a3f432d91b4dbba39641fb6781
lghtblb- <andrysds@gmail.com>
2016-12-19 19:30:46 +0700

    v.0.10a static map + remove unneeded file

--- js/script.js
+++ js/script.js
@@ -1,116 +1,20 @@
 var canvas;
 var ctx;
-
+var scaleLevel = 0.12;
+var lineWidth = 10;
 var map = new Image();
-var data;
 
-var pWidth;
-var pHeight;
+var schools;
+var fr = new FileReader();
 
 var vertices = [];
 var edges = [];
-
-var scale;
-var scaleLevel = 0;
-var fillScale;
-
-$(document).ready(function() {
-  canvas = $("#canvas")[0];
-  ctx = canvas.getContext("2d");
-
-  pWidth  = window.innerWidth;
-  pHeight = window.innerHeight;
-
-  $("#submit-btn").click(function() {
-    $("#input-data").hide();
-    $("#loader").show();
-
-    var mfr = new FileReader();
-    mfr.onload = function(e) {
-      map.src = mfr.result;
-      map.onload = function() {
-        computeFillScale();
-
-        var sfr = new FileReader();
-        sfr.onload = function(e) {
-          data = JSON.parse(sfr.result);
-
-          var schools = data.schools;
-          for (var i = 0; i < schools.length; i++) {
-            var vertex = new Vertex(
-              geoToMapX(schools[i].longitude),
-              geoToMapY(schools[i].latitude)
-            );
-            vertices.push(vertex);
-          }
-          edges = generateVoronoi(vertices, map.width, map.height);
-
-          drawTransformed();
-
-          $("#loader").hide();
-          $("#mycanvas").show();
-        };
-
-        var schools_file = $("#schools-input")[0].files[0];
-        sfr.readAsText(schools_file);
-      };
-    }
-
-    var map_file = $("#map-input")[0].files[0];
-    mfr.readAsDataURL(map_file);
-  });
-
-  $("#zoom-in-btn").click(function() {
-    scaleLevel++;
-    pWidth *= 1.5;
-    pHeight *= 1.5;
-    scale = fillScale * Math.pow(1.5, scaleLevel);
-    drawTransformed();
-  });
-
-  $("#zoom-out-btn").click(function() {
-    scaleLevel--;
-    pWidth /= 1.5;
-    pHeight /= 1.5;
-    scale = fillScale * Math.pow(1.5, scaleLevel);
-    drawTransformed();
-  });
-});
-
-function computeFillScale() {
-  var sf1 = pWidth / map.width;
-  var sf2 = pHeight / map.height;
-
-  if (sf1 < sf2) {
-    fillScale = sf1;
-    scale = fillScale;
-    pHeight = fillScale * map.height;
-  }
-  else {
-    fillScale = sf2;
-    scale = fillScale;
-    pWidth = fillScale * map.width;
-  }
-}
-
-function drawTransformed() {  
-  canvas.width = pWidth;
-  canvas.height = pHeight;
-  ctx.clearRect(0, 0, map.width, map.height);
-
-  ctx.save();
-  ctx.scale(scale, scale);
-  ctx.drawImage(map, 0, 0);
-  drawVertices(vertices);
-  drawEdges(edges);
-  ctx.restore();
-}
 
 function drawVertices(vertices) {
   ctx.fillStyle = "#F44336";
   for (i in vertices) {
     ctx.beginPath();
-    ctx.arc(vertices[i].x, vertices[i].y, 1 / scale * 3, 0, Math.PI * 2); 
+    ctx.arc(vertices[i].x, vertices[i].y, lineWidth * 2, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
   }
@@ -118,7 +22,7 @@ function drawVertices(vertices) {
 
 function drawEdges(edges) {
   ctx.strokeStyle = "#00b3fd";
-  ctx.lineWidth = 1 / scale * 2;
+  ctx.lineWidth = lineWidth;
   ctx.lineCap = 'round';
   for(i in edges) {
     ctx.beginPath();
@@ -128,10 +32,79 @@ function drawEdges(edges) {
   }
 }
 
+function drawTransformed() {
+  canvas.width = scaleLevel * map.width;
+  canvas.height = scaleLevel * map.height;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.save();
+  ctx.scale(scaleLevel, scaleLevel);
+  ctx.drawImage(map, 0, 0);
+  if (edges.length > 0) {
+    drawVertices(vertices);
+    drawEdges(edges);
+  }
+  ctx.restore();
+}
+
+map.onload = function() {
+  drawTransformed();
+
+  $("#loader").hide();
+  $("#wrapper").show();
+}
+
+fr.onload = function(e) {
+  vertices.length = 0;
+  edges.length = 0;
+
+  schools = JSON.parse(fr.result);
+
+  console.log(schools);
+
+  for (var i = 0; i < schools.length; i++) {
+    var vertex = new Vertex(
+      geoToMapX(schools[i].longitude),
+      geoToMapY(schools[i].latitude)
+    );
+    vertices.push(vertex);
+  }
+  edges = generateVoronoi(vertices, map.width, map.height);
+
+  drawTransformed();
+};
+
+$(document).ready(function() {
+  canvas = $("#canvas")[0];
+  ctx = canvas.getContext("2d");
+  map.src = "data/map.png";
+});
+
+$("#schools-input").change(function() {
+  var schools_file = $(this)[0].files[0];
+  fr.readAsText(schools_file);
+});
+
+$("#zoom-in-btn").click(function() {
+  if (scaleLevel < 1.5) {
+    scaleLevel *= 2;
+    lineWidth /= 2;
+    drawTransformed();
+  }
+});
+
+$("#zoom-out-btn").click(function() {
+  if (scaleLevel > 0.2) {
+    scaleLevel /= 2;
+    lineWidth *= 2;
+    drawTransformed();
+  }
+});
+
 function geoToMapX(x) {
-  return (x - data.area.x) * map.width / data.area.width;
+  return (x - 105) * 1200.2;
 }
 
 function geoToMapY(y) {
-  return (data.area.y - y) * map.height / data.area.height;
+  return (-5 - y) * 1200.2;
 }
